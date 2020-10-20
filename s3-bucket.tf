bd43019c9f3d650b454f9622d3b6638553addc86
Carl Wagner <dragonscelt@gmail.com>
2020-10-20 00:32:14 +0000

    Trying the other url

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/uopxCwagnerTfBootcamp/s3-bucket/aws"
+  source  = "https://github.com/Dragonscelt/terraform-aws-s3-bucket"
   version = "1.15.0"
   # insert required variables here
   bucket_prefix = "YupDiditWrong"
